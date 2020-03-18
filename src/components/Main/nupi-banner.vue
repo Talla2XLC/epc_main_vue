6f3e7401888f75c9ba5de2eb20977c243dda1c4c
Talla2XLC <alexey.ivanov@epc.com.ru>
2020-03-18 15:40:52 +0300

    Update nupi-banner.vue

--- src/components/Main/nupi-banner.vue
+++ src/components/Main/nupi-banner.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="nupi-banner">
     <span>Централизованный склад трубопровода Nupi в России</span>
+    <p>Спецальные цены для партнёров, сезонные акции, предложения и скидки!</p>
   </div>
 </template>
 
@@ -14,30 +15,37 @@ export default {
 
 .nupi-banner
   width: 50%
-  height: 50%
-  border: 1px solid red
+  height: 70%
+  border: 1px solid #ed001e
   border-radius: 2rem
   display: -webkit-flex
   display: -moz-flex
   display: -ms-flex
   display: -o-flex
   display: flex
+  flex-flow: column nowrap
   flex-grow: 1
   align-self: center
-  background: url("~@/assets/banners/nupi/tube.jpg") center no-repeat
-  background-size: contain
-  padding: 1rem 0
+  background: url("~@/assets/partners_logo/nupi_logo.png") center no-repeat
+  background-size: auto 40%
+  padding: 1rem 0.5rem
   box-sizing: border-box
-
+  cursor: pointer
+  text-align: center
+  transition: transform 200ms ease-in
   >span
-    text-align: center
+    font-family: "calibri", sans-serif
     flex-grow: 1
+    font-weight: 600
+    font-size: 1.1rem
+  >p
+    font-family: "calibri", sans-serif
+
+  &:hover
+    background-color: #dad7d7
+    transform: scale(1.05)
 
 @media (min-width: 900px)
-  .partners
-    padding: 0 10%
 
 @media (min-width: 1850px)
-  .partners
-    padding: 0.5rem 20%
 </style>
